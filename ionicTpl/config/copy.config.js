7e19c09b0e5368856dd69f3b59cc01027a30d881
Charles Grimont <grimont.c@gmail.com>
2020-09-25 13:39:34 +0200

    update animate css

--- ionicTpl/config/copy.config.js
+++ ionicTpl/config/copy.config.js
@@ -23,10 +23,6 @@ module.exports = {
         src: ['{{ROOT}}/node_modules/ionicons/dist/fonts/*', '{{ROOT}}/node_modules/ionic-angular/fonts/*'],
         dest: '../DisplayObjects/mobile/assets/fonts'
     },
-    copyAnimations :{
-        src: ['{{ROOT}}/node_modules/animate.css/animate.min.css'],
-        dest: '../DisplayObjects/mobile/assets/css'
-    },
     copyFlashUpdate: {
         src: ['../Flashupdate/index-fu.html', '../Flashupdate/flashupdate.js'],
         dest: '../DisplayObjects/mobile/'
