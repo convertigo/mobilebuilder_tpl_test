245c22e14e8bb4a5fd0650ed5ee798a056b24af9
Charles Grimont <grimont.c@gmail.com>
2020-10-12 10:07:49 +0200

    up

--- ionicTpl/src/app/main.ts
+++ ionicTpl/src/app/main.ts
@@ -2,4 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
+    if ('serviceWorker' in navigator) {
+        console.log("trying to register");
+        navigator.serviceWorker.register('ngsw-worker.js');
+      }
+    }).catch(err => console.log(err));
