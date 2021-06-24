4a38d3f7763c2c368606b13c5cbb7dd823c79109
Charles Grimont <grimont.c@gmail.com>
2021-06-24 11:08:08 +0200

    ref https://github.com/convertigo/C8oForms/issues/350 update sdk
    - to support new tinymce and with new sdk version

--- ionicTpl/config/copy.config.js
+++ ionicTpl/config/copy.config.js
@@ -1,9 +1,8 @@
-
 // https://www.npmjs.com/package/fs-extra
 
 module.exports = {
 
-    copyAssets :{
+    copyAssets: {
         src: ['{{SRC}}/assets/**/*'],
         dest: '../DisplayObjects/mobile/assets'
     },
@@ -11,7 +10,7 @@ module.exports = {
         src: ['{{SRC}}/index.html', '{{SRC}}/env.json'],
         dest: '../DisplayObjects/mobile/'
     },
-    copyServiceWorker:{
+    copyServiceWorker: {
         src: ['{{SRC}}/service-worker.js'],
         dest: '../DisplayObjects/mobile/'
     },
@@ -19,7 +18,7 @@ module.exports = {
         src: ['{{ROOT}}/node_modules/ionic-angular/polyfills/polyfills.js'],
         dest: '../DisplayObjects/mobile/build/'
     },
-    copyFonts :{
+    copyFonts: {
         src: ['{{ROOT}}/node_modules/ionicons/dist/fonts/*', '{{ROOT}}/node_modules/ionic-angular/fonts/*'],
         dest: '../DisplayObjects/mobile/assets/fonts'
     },
@@ -31,19 +30,20 @@ module.exports = {
         src: ['{{SRC}}/manifest.json'],
         dest: '../DisplayObjects/mobile/'
     },
-    copyCdvPlugins:
-    {
+    copyCdvPlugins: {
         src: '{{SRC}}/plugins.txt',
         dest: '../DisplayObjects/mobile/'
     },
-    copySwToolbox:
-    {
+    copySwToolbox: {
         src: '{{ROOT}}/node_modules/sw-toolbox/sw-toolbox.js',
         dest: '../DisplayObjects/mobile/build/'
     },
-    copySwAng:
-    {
+    copySwAng: {
         src: '{{ROOT}}/node_modules/@angular/service-worker/ngsw-worker.js',
         dest: '../DisplayObjects/mobile/'
+    },
+    copyTinyAssets: {
+        src: '{{ROOT}}/node_modules/tinymce/**/*',
+        dest: '../DisplayObjects/mobile/tinymce'
     }
 };
